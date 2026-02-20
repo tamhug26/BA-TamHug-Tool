4f68e728c031ba9af1c1c421adbf4cac93afdc8e
tamhug26 <tamara@huggler.net>
2026-02-20 10:27:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ st.title("PV Dimensionierungshilfe")
 
 #inputs:
 pv_Peakleistung = st.slider("PV-Peakleistung (kWp)", 0, 30, 10)
-Dachneigung = st.number_input("Dachneigung (°))", 0, 180, 360)
+Dachneigung = st.number_input("Dachneigung (°)", 0, 360, 180)
 #Dachausrichtung = 
 #Standort=
 batteriekapazität = st.slider("Batteriekapazität (kWh)", 0, 20, 10)
 maxLadeleistungBatterie = st.slider("max. Ladeleistung der Batterie (kW)", 0, 20, 10)
 maxEntladeleistungBatterie = st.slider("max. Entladeleistung der Batterie (kW)", 0, 20, 10)
-minSoC = st.number_input("Min. SoC (%))", 0, 20, 50)
-maxSoC = st.number_input("Max. SoC (%))", 60, 80, 100)
+minSoC = st.number_input("Min. SoC (%))", 0, 50, 20)
+maxSoC = st.number_input("Max. SoC (%))", 60, 100, 80)
 # regel einbauen minSoC muss < sein als maxSoC
-Einspeisegrenze = st.number_input("Eispeisegrenze (kW))", 60, 80, 100)
-Bezugsgrenze = st.number_input("Bezugsgrenze (kW))", 60, 80, 100)
+Einspeisegrenze = st.number_input("Eispeisegrenze (kW))", 60, 100, 80)
+Bezugsgrenze = st.number_input("Bezugsgrenze (kW))", 60, 100, 80)
 Jahresheizwärmebedarf = st.number_input("Jahresheizwärmebedarf (kWh/a)", 1000, 10000, 4500)
 jahresverbrauch = st.number_input("Jahresstrombedarf (kWh/a)", 1000, 10000, 4500)
 
