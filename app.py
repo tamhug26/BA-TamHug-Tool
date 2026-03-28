198a56f801a193601cbe6219a6e81bf4a53a10c8
tamhug26 <tamara@huggler.net>
2026-03-28 14:06:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
-st.write("test")
+st.write("test1")
 
 #https://ba-tamhug-tool-j82ipmep3hfrkgr36hxv9e.streamlit.app/#dimensionierungstool
 
