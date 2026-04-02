58c78e299e0e3c57b8ce921ced7fd91f56e3508f
tamhug26 <tamara@huggler.net>
2026-04-02 10:31:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1011,7 +1011,7 @@ fig_year.update_xaxes(
     tickformat="%b",
     dtick="M1"
 )
-fig_year.update_yaxes(range=[0, 550])
+fig_year.update_yaxes(rangemode="tozero")
 
 st.plotly_chart(fig_year, use_container_width=True)
 
