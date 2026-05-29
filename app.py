461e19ee095cc6118f6ccf738cf6914186a759e3
tamhug26 <tamara@huggler.net>
2026-05-29 19:50:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2268,6 +2268,7 @@ if "df_ts" in st.session_state:
         col1, col2 = st.columns(2)
 
         with col1: 
+            st.write("Treibhausgasemmissionen Data")
             st.dataframe(df_umwelt.round(2), use_container_width=True)
 
         with col2:
