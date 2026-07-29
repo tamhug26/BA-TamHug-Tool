8105ba1a952d059a2bf1c1d11e9383f7009ea2b0
tamhug26 <tamara@huggler.net>
2026-07-29 11:42:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -503,8 +503,8 @@ def get_display_dataframe(df, zeitraum, start_datum=None, start_monat=None):
 
         df_anzeige = df[(df.index >= start) & (df.index < ende)][spalten]
 
-        # Durchschnitt pro Stunde
-        #df_anzeige = df_anzeige.resample("15min").mean()
+        # Durchschnitt pro 15 minuten
+        df_anzeige = df_anzeige.resample("15min").mean()
     # zu täglichen Mittelwerten
     elif zeitraum == "Woche":
         if start_datum is None:
