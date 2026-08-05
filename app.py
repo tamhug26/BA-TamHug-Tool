c2e83adcc5103176cfb19b02a89e936dd0541d8d
tamhug26 <tamara@huggler.net>
2026-08-05 19:57:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3897,23 +3897,29 @@ if run_simulation:
 
         # Wirtschaftliche Kennzahlen der Anlage berechnen
         jahreskennzahlen["Eingesparte_Stromkosten_CHF"] = eingesparte_stromkosten
-        kostenkennzahlen = berechne_kostenkennzahlen(
-            df_ts=df_ts,
+        #hier
 
-            jahreskennzahlen=jahreskennzahlen,
-            pv_kwp_total=gesamt_pv_peakleistung,
-            batterie_aktiv=batterie_aktiv,
-            batteriekapazitaet=batteriekapazität,
-            pv_investition_brutto=pv_investition_brutto,
-            batteriekosten_chf=batteriekosten_chf,
-            optimierungskosten_chf=optimierungskosten_chf,
-            foerderanteil_pv_prozent=foerderanteil_pv_prozent,
-            betriebskosten_prozent=betriebskosten_prozent,
-            strompreis_chf_kWh=strompreis_chf_kWh,
-            ruecklieferverguetung_chf_kWh=ruecklieferverguetung_chf_kWh,
-            betrachtungsdauer_jahre=betrachtungsdauer_jahre,
-            wallboxkosten_chf=wallboxkosten_chf
-        )
+        try:
+            kostenkennzahlen = berechne_kostenkennzahlen(
+                df_ts=df_ts,
+                jahreskennzahlen=jahreskennzahlen,
+                pv_kwp_total=gesamt_pv_peakleistung,
+                batterie_aktiv=batterie_aktiv,
+                batteriekapazitaet=batteriekapazität,
+                pv_investition_brutto=pv_investition_brutto,
+                batteriekosten_chf=batteriekosten_chf,
+                optimierungskosten_chf=optimierungskosten_chf,
+                foerderanteil_pv_prozent=foerderanteil_pv_prozent,
+                betriebskosten_prozent=betriebskosten_prozent,
+                strompreis_chf_kWh=strompreis_chf_kWh,
+                ruecklieferverguetung_chf_kWh=ruecklieferverguetung_chf_kWh,
+                betrachtungsdauer_jahre=betrachtungsdauer_jahre,
+                wallboxkosten_chf=wallboxkosten_chf
+            )
+
+        except Exception as e:
+            st.exception(e)
+            st.stop()
 
         st.session_state["kostenkennzahlen"] = kostenkennzahlen
 
